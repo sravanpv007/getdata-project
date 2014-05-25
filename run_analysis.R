b3db7a1270c9bbcc6a3b36334926b55867b7c281
Volkan Unsal <spocksplanet@gmail.com>
2014-05-25 17:23:29 -0400

    add codebook, etc

--- run_analysis.R
+++ run_analysis.R
@@ -1,25 +1,55 @@
 
 # Merges the training and the test sets to create one data set.
-activity_labels <- read.table("./dataset/activity_labels.txt", col.names=c("id","name"))
+# LOAD THE DATA
 features <- read.table("./dataset/features.txt", col.names=c("id","name"))
 
-x_train <- read.table("./dataset/train/X_train.txt", col.names=features$name)
-y_train <- read.table("./dataset/train/y_train.txt", col.names=c("activity_id"))
-subject_train <- read.table("./dataset/train/subject_train.txt", col.names=c("subject_id"))
+train.set <- read.table("./dataset/train/X_train.txt", col.names=features$name)
+train.labels <- read.table("./dataset/train/y_train.txt", col.names=c("activity.id"))
+train.subject <- read.table("./dataset/train/subject_train.txt", col.names=c("subject.id"))
 
-x_test <- read.table("./dataset/test/X_test.txt", col.names=features$name)
-y_test <- read.table("./dataset/test/y_test.txt", col.names=c("activity_id"))
-subject_test <- read.table("./dataset/test/subject_test.txt", col.names=c("subject_id"))
-
-# TODO: Make the x_train the columns and the y_train the labels of the columns in a new dataframe. The rows will be identified by features.
-# TODO: merge the data frames by their common identifiers, namely the column names
-
+# LOAD THE DATA
+test.set <- read.table("./dataset/test/X_test.txt", col.names=features$name)
+test.labels <- read.table("./dataset/test/y_test.txt", col.names=c("activity.id"))
+test.subject <- read.table("./dataset/test/subject_test.txt", col.names=c("subject.id"))
 
 # Extracts only the measurements on the mean and standard deviation for each measurement.
+# SUBSET THE DATASETS
+nm <- colnames(train.set)
+mean_or_std <- grepl("(mean|std)\\.\\.", nm)
+train.subset <- train.set[mean_or_std]
+test.subset <- test.set[mean_or_std]
+
+# CREATE NEW VARIABLES
+# Activity Id
+train.subset$activity.id <- train.labels$activity.id
+test.subset$activity.id <- test.labels$activity.id
+# Subject Id
+train.subset$subject.id <- train.subject$subject.id
+test.subset$subject.id <- test.subject$subject.id
+# Origin
+train.subset$origin <- "train"
+test.subset$origin <- "test"
+
+# COMBINE THE DATASETS
+combo.sets <- rbind(train.subset,test.subset)
+
+
 
 # Uses descriptive activity names to name the activities in the data set
-
 # Appropriately labels the data set with descriptive activity names.
+# LOAD DATA
+activity.labels <- read.table("./dataset/activity_labels.txt", col.names=c("activity.id","activity.label"))
+combo.sets <- merge(combo.sets,activity.labels, by="activity.id")
+
 
 # Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+library(reshape2)
+nm.combo <- colnames(combo.sets)[!(nm.combo %in% c("activity.id","subject.id","origin","activity.label"))]
+combo.melt <- melt(combo.sets, id=nm.combo, measure.vars=c("activity.id","subject.id"))
+
+
+combo.tidy <- dcast(combo.melt, nm.combo ~ variable, mean)
+
+# SAVE THE TIDY DATASET
+write.table(combo.tidy,"tidy.txt")
 
